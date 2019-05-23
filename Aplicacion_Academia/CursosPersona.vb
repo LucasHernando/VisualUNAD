6128af3789cb7d92c3919c2c64e31959cba5e004
LucasHernando <47490470+LucasHernando@users.noreply.github.com>
2019-05-23 00:46:00 -0500

    Cambios Evaluacion
    Cambios Evaluacion

--- Aplicacion_Academia/CursosPersona.vb
+++ Aplicacion_Academia/CursosPersona.vb
@@ -1,5 +1,4 @@
 ﻿Public Class CursosPersona
-<<<<<<< HEAD
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim Integelia As New Form5()
         Integelia.Show()
@@ -8,7 +7,4 @@
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
         Me.Close()
     End Sub
-=======
-
->>>>>>> 5897d9098a136f17c1f788f2fdc1125f3ba50606
 End Class
